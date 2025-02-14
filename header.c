1ef6480d93cbbc77eda4157537434b64748e452f
DottorB <enricobucci210@gmail.com>
2025-02-14 09:34:06 +0100

    corretto e aggiunto alcuni commenti, eliminate le variabili inutilizzate, migliorata l'organizzazione.

--- header.c
+++ header.c
@@ -3,53 +3,54 @@
 #include "header.h"
 
 
-//inizializzo l'array di 100 dischi con 6 dischi iniziali contenenti tutte le informazioni
+//inizializzo un array di struct di 100 dischi con 6 dischi iniziali contenenti tutte le informazioni
 disco archivio[100] = {
+    //nome, autore o band, costo di noleggio, codice, presenza
     {"I Want to Break Free", "Queen", 15.20, 1, 1},
     {"Back in Black", "ACDC", 14.40, 2, 1},
     {"Doomsday", "MF DOOM", 20.50, 3, 1},
     {"Whatever It Takes", "Imagine Dragons", 18.50, 4, 1},
-    {"Bordeline", "Tame Impala", 19.90, 5, 1},
+    {"Borderline", "Tame Impala", 19.90, 5, 1},
     {"Don't Stop Me Now", "Queen", 20.00, 6, 1}
 };
 
 
 void aggiungi_disco(int *disco_nuovo, int *n_codice) {
-    //se i dischi sono minori di 100 allora si possono aggiungere
+    //controllo della quantità di dischi, se sono minori di 100, si può continuare
     if (*disco_nuovo < 100) {
+        //inserimento titolo del disco
         printf("Inserisci il titolo del disco \n");
         fgets(archivio[*disco_nuovo].titolo, 50,stdin);
         archivio[*disco_nuovo].titolo[strcspn(archivio[*disco_nuovo].titolo, "\n")] = '\0';
-
+        //inserimento autore o band
         printf("inserisci l'autore o la band del disco \n");
         fgets(archivio[*disco_nuovo].autore_band, 50, stdin);
         archivio[*disco_nuovo].autore_band[strcspn(archivio[*disco_nuovo].autore_band, "\n")] = '\0';
-
+        //inserimento prezzo del noleggio
         printf("inserisci il prezzo del  disco \n");
         scanf("%f", &archivio[*disco_nuovo].costo_prestito);
         getchar();
-
+        //chiamata alla funzione assegna codice, per associare al disco aggiunto un codice
         archivio[*disco_nuovo].disponibile = 1;
-        (*disco_nuovo)++;
         assegna_codice(n_codice, disco_nuovo);
+        (*disco_nuovo)++;
     } else //se i dischi sono 100 non si possono più aggiungere
     {
-        printf("il nostro archivio e' pieno! non e' possibile aggiungere ulterior dischi \n");
+        //messaggio che avvisa l'utente che è stato superato il limite di dischi
+        printf("il nostro archivio e' pieno! non e' possibile aggiungere ulteriori dischi \n");
     }
 }
 
 //assegna automaticamente il codice a un disco
 void assegna_codice(int *n_codice, int *disco_nuovo) {
-    char scelta;
-
-    archivio[*disco_nuovo].codice = *n_codice; //aumenta il puntatore legato al numero del codice
-
+    archivio[*disco_nuovo].codice = *n_codice;
+    //indica il codice del disco
     printf("il codice del disco e' %d \n", *n_codice);
-
+    //aumenta ogni volta il valore numerico del codice
     (*n_codice)++;
 } //permette la visualizzazione di tutti i dischi presenti nell'archivio
 void visualizza_dischi(int *disco_nuovo) {
-    //tramite due cicli annidati divido i dischi per autore o band
+    //tramite due cicli annidati organizza i dischi per autore o band
     for (int i = 0; i < *disco_nuovo; i++) {
         for (int j = i + 1; j < *disco_nuovo; j++) {
             if (strcmp(archivio[i].autore_band, archivio[j].autore_band) > 0) {
@@ -58,7 +59,7 @@ void visualizza_dischi(int *disco_nuovo) {
                 archivio[j] = var_temp;
             }
         }
-    } //tramite altri due cicli annidati faccio si che anche i codici siano sistemati
+    } //tramite altri due cicli annidati fa si che anche i codici siano sistemati
     for (int i = 0; i < *disco_nuovo; i++) {
         for (int j = i + 1; j < *disco_nuovo; j++) {
             if (archivio[i].codice > archivio[j].codice) {
@@ -68,7 +69,7 @@ void visualizza_dischi(int *disco_nuovo) {
             }
         }
     }
-    //con u for procedo all visualizzazione di tutti i dischi
+    //con un ciclo for procedo all visualizzazione di tutti i dischi
     for (int i = 0; i < *disco_nuovo; i++) {
         printf("Titolo:%s \n", archivio[i].titolo);
         printf("Autore/Band:%s \n", archivio[i].autore_band);
@@ -78,59 +79,59 @@ void visualizza_dischi(int *disco_nuovo) {
     }
 }
 
-//fuznione che permette di "noleggiare" un disco se è presente
-void preleva_disco(int trovato, int *disco) {
+//funzione che permette di "noleggiare" un disco se è presente
+void preleva_disco(int trovato, int *disco_nuovo) {
     char nome_disco[50];
 
     printf("inserire il nome del disco da noleggiare \n");
-    //tramite fgets si possono scrivere anche titoli di canzoni con più parole
+    //tramite "fgets" si possono scrivere anche titoli di canzoni con più parole
     fgets(nome_disco, 50,stdin);
-    //tramite strcspn facciamo si che il carattere \n venga sostituito da \0 così che non venga visualizzato
+    //tramite "strcspn" fa si che il carattere "\n" venga sostituito da "\0" così che non venga visualizzato
     nome_disco[strcspn(nome_disco, "\n")] = '\0';
 
     trovato = 0;
 
-    for (int i = 0; i < *disco; i++) {
-        //tramite strcmp si controlla se il disco scritto corrisponda a quello dell'archivio
+    for (int i = 0; i < *disco_nuovo; i++) {
+        //tramite strcmp si controlla se il disco scritto corrisponda a un disco presente nell'archivio
         if (strcmp(nome_disco, archivio[i].titolo) == 0) {
             trovato = 1;
 
-            // se è disponibile viene noleggiato e la variabile della disponibilità viene azzerata (non è possibile riprenderlo)
+            // se è disponibile viene noleggiato e la variabile della disponibilità viene azzerata (non è possibile noleggiarlo di nuovo)
             if (archivio[i].disponibile == 1) {
                 printf("disco noleggiato con successo, goditi la tua musica preferita \n");
 
                 archivio[i].disponibile = 0;
             } else {
-                //se non c'è un messaggio di testo ne avviserà la mancanza
+                //se non c'è il disco desiderato, un messaggio di testo avviserà l'utente che non può noleggiarlo
                 printf("ci dispiace, purtroppo il disco e' stato gia' noleggiato \n");
             }
             break;
         }
     }
 
-    if (!trovato) // se i nomi non corrispondono, si ricerca
+    if (!trovato) // se il nome non è presente nell'archivio si cerca di nuovo
     {
         printf("disco non trovato, riprovi per favore \n");
 
-        preleva_disco(trovato, disco);
+        preleva_disco(trovato, disco_nuovo);
     }
 }
 
-//funzione che confronta tutti i dischi per trovare il più costsoso tramite un approccio ricorsivo e incrementale
+//funzione che confronta tutti i dischi per trovare il più costoso tramite un approccio ricorsivo e incrementale
 void piu_costoso(int *disco, int indice, int *indice_max, float *max_costo) {
-    //esaminati tutti i dischi presenta il messaggio con il disco più costoso
+    //esaminati tutti i dischi, presenta il messaggio con il disco più costoso
     if (indice == *disco) {
         printf("il disco con il costo maggiore e' %s ", archivio[*indice_max].titolo);
 
         printf("con il costo di %.2f \n", *max_costo);
         return;
-    } //sostituisco il disco maggiore se c'è un altro di costo ancora più alto
+    } //sostituisce il disco più costoso, se ne rivela un altro
     if (archivio[indice].costo_prestito > *max_costo) {
-        //sostituisco l'indice
+        //sostituisce l'indice
         *indice_max = indice;
-        //sostituisco il costo
+        //sostituisce il costo
         *max_costo = archivio[indice].costo_prestito;
     }
-    //tramite chiamata ricorsiva aumento l'indice fino a quando non finiscono i cofronti
+    //tramite chiamata ricorsiva aumenta l'indice fino a quando non finiscono i confronti
     piu_costoso(disco, indice + 1, indice_max, max_costo);
 }
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-/*gestione automatizzata di un archivio di dischi musicali. La capienza massima è di 100 dischi
-divisi tutti tramite titolo, nome, autore o gruppo a cui appartengono.
-Ci sarà la possibilità di inserire dischi nuovi, visualizzare quelli presenti di
-uno stesso autore o gruppo e cercare nell'archivio il disco più costoso.*/
+/* gestione automatizzata di un archivio di dischi musicali. La capienza massima è di 100 dischi
+divisi tutti in titolo, nome, autore o gruppo a cui appartengono.
+Ci sarà la possibilità di inserire dischi nuovi, visualizzare quelli presenti organizzati in base
+allo stesso autore o gruppo e cercare nell'archivio il disco più costoso.*/
 #include<stdio.h>
 #include "header.h"
 
@@ -14,28 +14,30 @@ int main() {
 
     printf("Benvenuto nel negozio di dischi di Enrico Bucci \n");
 
-    int disco = 6; //rappresenta i dischi presenti
-    int n_codice = disco++ ; //rappresenta il codice
-    int trovato, massimo;
-    //rappresenta una variabile che userò nel noleggio e una per la classificazione del disco più costoso
-    int continua = 1; //rappresenta una variabile che permette di fare più operazioni
-
+    int disco = 6; //variabile che indica i dischi già presenti
+    int n_codice = disco + 1; //variabile che indica il codice di ciascun disco
+    int trovato;
+    int continua = 1; //variabile che permette di fare 2 o più operazioni prima della chiusura del programma
+    //gestisce la continuità del programma
     while (continua) {
         printf("Selezionare un'opzione \n");
 
-        //tramite una serie di printf presento i comandi legati alle varie operazioni
+        //tramite una serie di printf si presentano i comandi legati alle varie operazioni
         printf("-digitare 1 se si vuole inserire un nuovo disco all'archivio (aggiungi i tuoi gusti!)\n");
         printf("-digitare 2 se si vuole visualizzare tutti i dischi (cerca il disco che piu' ti piace!)\n");
         printf("-digitare 3 se si desidera prendere in prestito un disco(approfitta finche' e' ancora in negozio!)\n");
         printf("-digitare 4 se si desidera sapere il disco piu' costoso (scopri il disco piu' costoso che abbiamo!)\n");
         printf("-digitare 0 se non si desidera fare alcuna operazione \n");
 
+        //con scanf si registra la risposta dell'utente
         scanf("%d", &scelta_operazione);
 
-        //pulisce il buffer, così da poter usare fgets nella fuzione aggiungi_disco
+        //pulisce il buffer, così da poter usare "fgets" nella funzione aggiungi_disco
         getchar();
-        //utilizzo uno switch per la selezione delle operazioni
+        //utilizzo uno switch per la selezione dell'operazione desiderata
         switch (scelta_operazione) {
+            case 0: printf("grazie per averci scelti arrivederci! \n"); //chiude il programma
+                return 0;
             case 1: aggiungi_disco(&disco, &n_codice);
                 break;
             case 2: visualizza_dischi(&disco);
@@ -44,19 +46,20 @@ int main() {
                 break;
             case 4: piu_costoso(&disco, indice, &indice_max, &max_costo);
                 break;
+            //messaggio di errore se viene scritto un numero diverso da quelli indicati
             default: printf("Operazione non valida. Riprova.\n");
                 break;
         }
 
 
-        //tramite printf presento il comando per poter coninuare o meno le operazioni
-        printf("premi 1 se desideri fare un'altra operazione, premi 0 se desideri chiuere il programma \n");
+        //messaggio che pone all'utente la possibilità di continuare o meno l'esecuzione del programma
+        printf("premi 1 se desideri fare un'altra operazione, premi 0 se desideri chiudere il programma \n");
 
         scanf("%d", &continua);
 
-        if (continua == 0) //se è uguale a 0 finisce il programma
+        if (continua == 0) //se si imposta la variabile uguale a 0 finisce il programma
         {
-            printf("grazie per averci scelti arrivederci! \n");
+            printf("grazie per averci scelti arrivederci! \n"); //messaggio di chiusura del programma
         }
     }
 }
